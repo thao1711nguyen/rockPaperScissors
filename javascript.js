a60d69b1b6c11f600562b85aa69b6daa6006ca35
Thao Nguyen <thao1711nguyen@gmail.com>
2022-12-14 20:11:43 +0700

    function to play one round of the game

--- javascript.js
+++ javascript.js
@@ -3,14 +3,49 @@ function getComputerChoice (){
     let a;
     switch (c){
         case 0:
-            a='Rock';
+            a='rock';
             break;
         case 1:
-            a='Paper';
+            a='paper';
             break;
         default:
-            a='Scissor';
+            a='scissors';
         
     }
     return a;
+}
+let computerSelection = getComputerChoice();
+let playerSelection = prompt('Rock, paper or scissors?');
+
+function playRound (playerSelection, computerSelection){
+    playerSelection.toLocaleLowerCase();
+    if(playerSelection == computerSelection){
+        return 'We tie'
+    } else if(playerSelection == 'rock'){
+        switch (computerSelection){
+            case 'paper':
+                return 'You win!';
+                break;
+            default:
+                return 'You lose!';
+        }
+
+    } else if(playerSelection == 'paper'){
+        switch (computerSelection){
+            case 'rock':
+                return 'You win!';
+                break;
+            default:
+                return 'You lose!';
+        }
+    } else {
+        switch (computerSelection){
+            case 'rock':
+                return 'You lose!';
+                break;
+            default:
+                return 'You win!';
+        }
+    }
+    
 }
